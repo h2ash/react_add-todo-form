c66986a3ba68aca2939584960334971fb2c956e4
h2ash <hun22ash@gmail.com>
2019-07-17 21:32:16 +0300

    Updates

---
+++ precache-manifest.cdf6661ef35f27076762ad0f50f57031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ab3b65875794a77116b7517ab3b0a6",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "54ba2dda7ddbb2455dab",
+    "url": "/react_add-todo-form/static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_add-todo-form/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "54ba2dda7ddbb2455dab",
+    "url": "/react_add-todo-form/static/js/main.32120201.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
